1ee0cfabbad2e1b3dae13bc50d9c153299a3b3cd
Nigel Banks <nigel.g.banks@gmail.com>
2020-12-10 17:07:34 +0000

    Changed default allocator as it is significantly faster (5 times faster) when targeting musl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#[cfg(not(target_env = "msvc"))]
+use jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 mod args;
 
 use args::*;
